adf238ad4477f16d0724f5a653dee03e9109b5e4
leon <li.leon.yang@gmail.com>
2012-10-08 15:39:00 +0100

    a

--- src/server/conf/ConfigManager.java
+++ src/server/conf/ConfigManager.java
@@ -11,6 +11,7 @@ import java.io.File;
 
 public class ConfigManager {
 
+	//common
     private final String fileCommon = "common.properties";
     private final String fileVersion = "version";
     private Properties prop = null;
